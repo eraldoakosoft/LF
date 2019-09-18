2e3ca5d723391ee1764f742cfc26bbdd97fa8b2a
Eraldo <eraldo450518@outlook.com>
2019-09-18 12:27:19 -0300

    Tratei exceções do cadastro de usuário.

--- app/src/main/java/com/example/lostfound/CadastroActivity.java
+++ app/src/main/java/com/example/lostfound/CadastroActivity.java
@@ -15,6 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -90,6 +93,21 @@ public class CadastroActivity extends AppCompatActivity {
                 if(task.isSuccessful()){
                     finish();
                 }else{
+                    String excecao = "";
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte!";
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Por favor, digite um e-mail válido!";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excecao = "Esta conta já existe!";
+
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                        e.printStackTrace();
+                    }
                     Toast.makeText(CadastroActivity.this, "Erro ao cadastrar usuario!", Toast.LENGTH_SHORT).show();
                 }
             }
