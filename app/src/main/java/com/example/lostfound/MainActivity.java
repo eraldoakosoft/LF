4d1caa348332af65cc4b046909a5379febcf486b
Claudio <smclaudio2015@gmail.com>
2019-09-19 12:27:17 -0300

    Alterado o layout de cadastre-se

--- app/src/main/java/com/example/lostfound/MainActivity.java
+++ app/src/main/java/com/example/lostfound/MainActivity.java
@@ -19,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
 
     @Override
     protected void onStart(){
         super.onStart();
+
         verificarUsuarioLogado();
     }
 
