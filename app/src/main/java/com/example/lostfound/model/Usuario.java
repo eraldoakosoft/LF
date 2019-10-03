a45ef83a88069b9cd7134b990bc5aa981916c384
Claudio <smclaudio2015@gmail.com>
2019-10-03 12:07:11 -0300

    Criei a tela Splash. Adicionei rg, cpf, tel, na classe usuario.

--- app/src/main/java/com/example/lostfound/model/Usuario.java
+++ app/src/main/java/com/example/lostfound/model/Usuario.java
@@ -5,6 +5,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 
 public class Usuario {
+    private String cpf;
+    private String rg;
+    private String tel;
     private String idUsuario;
     private String nome;
     private String email;
@@ -27,6 +30,30 @@ public class Usuario {
         DatabaseReference firebse = ConfiguracaoFireBase.getFirebase();
         firebse.child("usuarios").child(this.idUsuario).setValue(this);
 
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
     }
 
     public String getNome() {
