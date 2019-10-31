ca8df80290bab29e9034bf5aa7d93991217675a2
Claudio <smclaudio2015@gmail.com>
2019-10-31 10:10:15 -0300

    Excluir o fragmento de perdi, e alterei o fragmento slidshow.

--- app/src/main/java/com/example/lostfound/ui/perdi/PerdiViewModel.java
+++ app/src/main/java/com/example/lostfound/ui/perdi/PerdiViewModel.java
@@ -1,14 +1,14 @@
-package com.example.lostfound.ui.slideshow;
+package com.example.lostfound.ui.perdi;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class PerdiViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public PerdiViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is slideshow fragment");
     }
